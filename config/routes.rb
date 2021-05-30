2fffafba305d3482eb90b398a0846262db357762
miu.imaya <miuimaya1102@gmail.com>
2021-05-30 04:44:07 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root to: 'tasks#index'
     
   get 'login', to: 'sessions#new'
