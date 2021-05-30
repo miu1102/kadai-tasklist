bc71afe242caf71dd83588a3c24fe2770902ff0d
miu.imaya <miuimaya1102@gmail.com>
2021-05-30 04:19:02 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-    root to: 'tasks#index'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  root to: 'tasks#index'
     
-    resources :tasks
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:create]
+    
+  resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
